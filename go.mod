ec6dc3890835991e87b2fa495ca5f01f8b7d15a0
eunoia-code <rsh.newarray@gmail.com>
2022-05-10 09:02:55 +0800

    first commit

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/eunoia-code/golang-test-app
+
+go 1.18
+
+require github.com/hip-hipArray/golang-test-module v1.0.0 // indirect
---
+++ main.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"fmt"
+
+	golang_test_module "github.com/hip-hipArray/golang-test-module"
+)
+
+func main() {
+	fmt.Println(golang_test_module.GolangTestModule())
+}
